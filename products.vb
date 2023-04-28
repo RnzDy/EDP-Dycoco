45538d6d387732520e78f5c2eaae604de1081755
Renz Domini Dycoco <renzdycoco@gmail.com>
2023-04-28 22:36:39 +0800

    New changes for Act 6 EDP

--- products.vb
+++ products.vb
@@ -9,7 +9,6 @@ Public Class products
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         main.Show()
         Close()
-
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
